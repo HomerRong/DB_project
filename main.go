5b108653a5927e3985b0a58008b16c239c190931
HomerRong <2560117937@qq.com>
2021-12-08 00:24:28 +0800

    feat:增加路由层，简单的图片上传和url接口

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"dbproject/mod/db_model"
+	"dbproject/mod/routers"
 	_ "encoding/json"
 	_ "gitee.com/opengauss/openGauss-connector-go-pq"
+	"log"
 )
 
 // Test used to test
@@ -12,13 +13,31 @@ type Test struct {
 	Content string `json:"content"`
 }
 
+//func LoginAdmin(c *gin.Context){
+//	c.JSON(200, gin.H{
+//		"message": "Hello 登录成功!",
+//	})
+//}
+
 func main() {
 
 	//test
-	var tmp Test
-	db_model.SetupDb()
-	db_model.Db.Where("id = ?", 2).First(&tmp)
-	//db_model.Db.First(&tmp)
-	println(tmp.Id)
-	println(tmp.Content)
+	//var tmp Test
+	//db_model.SetupDb()
+	//db_model.Db.Where("id = ?", 2).First(&tmp)
+	////db_model.Db.First(&tmp)
+	//println(tmp.Id)
+	//println(tmp.Content)
+	//router := gin.Default()
+	//
+	//
+	//router.POST("login", LoginAdmin)
+	//router.Run("127.0.0.1:9000")
+
+	router := routers.InitRouter()
+	err := router.Run("127.0.0.1:9000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
---
+++ routers/api/upload.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+// UploadImg 上传图片的api
+// 图片保存到目录下的pic文件夹中
+func UploadImg(c *gin.Context) {
+	var err error
+	file, err := c.FormFile("file")
+	if err != nil {
+		log.Fatalf("uploadImg error: %v", err)
+	}
+	log.Printf("loadimg: %v", file.Filename)
+
+	// 上传文件至指定目录
+	err = c.SaveUploadedFile(file, "./pic/"+file.Filename)
+	if err != nil {
+		log.Fatalf("uploadImg error: %v", err)
+	}
+
+	//@ToDO 将图片路径保存至数据库
+
+	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+}
